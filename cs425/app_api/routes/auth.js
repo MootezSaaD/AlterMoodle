8b5b19ac9304f0c17a3f893fd6ba2930e0a7c96b
MootezSaaD <mootez.saad@medtech.tn>
2020-03-05 11:54:16 +0100

    [WIP] User authentication part 2

--- cs425/app_api/routes/auth.js
+++ cs425/app_api/routes/auth.js
@@ -1,71 +1,83 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const {
-  signupValidation,
-  loginValidation
+    signupValidation,
+    loginValidation
 } = require("../validators/joiValidator");
 const bcrypt = require("bcryptjs");
 
 // Signup
-router.post("/signup", async (req, res) => {
-  // Validating the user's reponse before creating and storing the user
-  const { error } = signupValidation(req.body);
-  if (error)
-    return res.status(400).send({
-      success: false,
-      message: error.details[0].message
+router.post("/signup", async(req, res) => {
+    // Validating the user's reponse before creating and storing the user
+    const { error } = signupValidation(req.body);
+    if (error)
+        return res.status(400).send({
+            success: false,
+            message: error.details[0].message
+        });
+
+    // Check whether the user exists or not
+    const userEmail = User.findOne({ email: req.body.email });
+    if (userEmail)
+        return res.status(400).send({
+            success: false,
+            message: "User already exists"
+        });
+
+    // Hashing the password
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+    // Creating a new user.
+    const user = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        moodleToken: req.body.moodleToken,
+        password: hashPassword
     });
 
-  // Check whether the user exists or not
-  const userEmail = User.findOne({ email: req.body.email });
-  if (userEmail)
-    return res.status(400).send({
-      success: false,
-      message: "User already exists"
-    });
-
-  // Hashing the password
-  const salt = await bcrypt.genSalt(10);
-  const hashPassword = await bcrypt.hash(req.body.password, salt);
-  // Creating a new user.
-  const user = new User({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    moodleToken: req.body.moodleToken,
-    password: hashPassword
-  });
-
-  try {
-    const savedUser = await user.save();
-    res.status(201).send({
-      success: true,
-      message: "Your account has been registered"
-    });
-  } catch (err) {
-    res.status(400).send({
-      success: true,
-      message: err
-    });
-  }
+    try {
+        const savedUser = await user.save();
+        res.status(201).send({
+            success: true,
+            message: "Your account has been registered"
+        });
+    } catch (err) {
+        res.status(400).send({
+            success: true,
+            message: err
+        });
+    }
 });
 
 // Login
 router.post("/login", (req, res) => {
-  // Validating the user's reponse before creating and storing the user
-  const { error } = loginValidation(req.body);
-  if (error)
-    return res.status(400).send({
-      success: false,
-      message: error.details[0].message
-    });
-  // Check whether the user exists or not
-  const userEmail = User.findOne({ email: req.body.email });
-  if (!userEmail)
-    return res.status(400).send({
-      success: false,
-      message: "User does not exists"
-    });
+    // Validating the user's reponse before creating and storing the user
+    const { error } = loginValidation(req.body);
+    if (error)
+        return res.status(400).send({
+            success: false,
+            message: error.details[0].message
+        });
+    // Check whether the user exists or not
+    const userAccount = User.findOne({ email: req.body.email });
+    if (!userAccount)
+        return res.status(400).send({
+            success: false,
+            message: "User does not exists"
+        });
+    // Check whether the entered password is valid or not
+    const validAccount = bcrypt.compare(req.body.password, userAccount.password);
+    if (!validAccount)
+        return res.status(400).send({
+            success: false,
+            message: "Wrong password"
+        });
+    else
+        return res.status(200).send({
+            success: true,
+            message: "Welcome !"
+        });
 });
 
 module.exports = router;
--- cs425/app_api/validators/joiValidator.js
+++ cs425/app_api/validators/joiValidator.js
@@ -2,30 +2,45 @@ const Joi = require('@hapi/joi');
 
 // Signup Validation
 const signupValidation = data => {
-  const schema = Joi.object({
-    firstName: Joi
-      .string()
-      .min(2)
-      .required(),
-    lastName: Joi
-      .string()
-      .min(2)
-      .required(),
-    email: Joi
-      .string()
-      .min(6)
-      .required()
-      .email(),
-    password: Joi
-      .string()
-      .min(6)
-      .required(),
-    moodleToken: Joi
-      .string()
-      .required()
-  });
-  return schema.validate(data);
+    const schema = Joi.object({
+        firstName: Joi
+            .string()
+            .min(2)
+            .required(),
+        lastName: Joi
+            .string()
+            .min(2)
+            .required(),
+        email: Joi
+            .string()
+            .min(6)
+            .required()
+            .email(),
+        password: Joi
+            .string()
+            .min(6)
+            .required(),
+        moodleToken: Joi
+            .string()
+            .required()
+    });
+    return schema.validate(data);
+}
+
+// Login Validation
+const loginValidation = data => {
+    const schema = Joi.object({
+        email: Joi
+            .string()
+            .min(6)
+            .required(),
+        password: Joi
+            .string()
+            .min(6)
+            .required()
+    });
 }
 
 
 module.exports.signupValidation = signupValidation;
+module.exports.loginValidation = loginValidation;
