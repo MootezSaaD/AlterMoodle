9151ce6db6e32e97803326386d2342a394ba7f37
Mootez <mootez.saad@medtech.tn>
2020-03-06 16:28:01 +0100

    Create dashboard sidebar

--- cs425/src/app/app.module.ts
+++ cs425/src/app/app.module.ts
@@ -1,22 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { SideBarComponent } from './side-bar/side-bar.component';
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { SideBarComponent } from "./side-bar/side-bar.component";
+import { SignupComponent } from "./signup/signup.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     DashboardComponent,
-    SideBarComponent
+    SideBarComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
@@ -24,12 +24,20 @@ import { SideBarComponent } from './side-bar/side-bar.component';
     FormsModule,
     RouterModule.forRoot([
       {
-        path: 'dashboard',
+        path: "dashboard",
         component: DashboardComponent
+      },
+      {
+        path: "login",
+        component: LoginComponent
+      },
+      {
+        path: "signup",
+        component: SignupComponent
       }
     ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
