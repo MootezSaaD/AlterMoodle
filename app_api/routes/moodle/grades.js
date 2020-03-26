456b7b6fc5dddfa4a1bb728b77a5cc00c27b660b
Mootez <mootez.saad@medtech.tn>
2020-03-26 15:27:06 +0100

    Create API that returns user's overall grades in all courses

---
+++ app_api/routes/moodle/grades.js
@@ -0,0 +1,33 @@
+const Router = require("express").Router;
+const { verifyJwt } = require("../../helpers/verifyToken");
+const moodleService = require("../../services/moodle.service")();
+const Course = require("../../models/Course");
+const userService = require("../../services/user.service")();
+const transcriptService = require("../../services/transcript.service")();
+
+const router = Router({
+  mergeParams: true
+});
+
+/**
+ * Get user's overall grades
+ */
+router.get("/grades", verifyJwt, async (req, res) => {
+  const user = await userService.getUserByID(req.decodedToken._id);
+  console.log("Getting the course Arr", user.moodleUserID);
+  const courseArr = await transcriptService.fetchUsersCourses(
+    user.moodleUserID,
+    user.moodleToken
+  );
+  const gradesArr = await transcriptService.getUserGrades(
+    user.moodleUserID,
+    user.moodleToken
+  );
+  const transcript = await transcriptService.getTranscript(
+    courseArr,
+    gradesArr
+  );
+  res.status(200).send(transcript);
+});
+
+module.exports = router;
---
+++ app_api/services/transcript.service.js
@@ -0,0 +1,76 @@
+require("dotenv").config();
+const userService = require("../services/user.service")();
+const moodle = require("moodle-client");
+const User = require("../models/User");
+const Course = require("../models/Course");
+const { ErrorHandler } = require("../helpers/errorHandler");
+const _ = require("lodash");
+
+function transcriptService() {
+  // Get user's enrolled courses (following the Course Model)
+  async function fetchUsersCourses(userID, moodleToken) {
+    const res = moodle
+      .init({
+        wwwroot: process.env.MOODLE_URL,
+        token: moodleToken
+      })
+      .then(client => {
+        return client
+          .call({
+            wsfunction: "core_enrol_get_users_courses",
+            args: {
+              userid: userID
+            }
+          })
+          .then(async courses => {
+            let coursesArr = [];
+            courses.forEach(course => {
+              coursesArr.push({
+                courseCode: course.shortname,
+                courseid: course.id,
+                courseDesc: course.fullname
+              });
+            });
+            return coursesArr;
+          });
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+    return Promise.resolve(res);
+  }
+  // Get user's grades from moodle
+  async function getUserGrades(userID, moodleToken) {
+    const res = moodle
+      .init({
+        wwwroot: process.env.MOODLE_URL,
+        token: moodleToken
+      })
+      .then(client => {
+        return client
+          .call({
+            wsfunction: "gradereport_overview_get_course_grades",
+            args: {
+              userid: userID
+            }
+          })
+          .then(async grades => {
+            return grades.grades;
+          });
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+    return Promise.resolve(res);
+  }
+  // Return users transcript
+  async function getTranscript(courseArr, gradesArr) {
+    const transcript = _.map(courseArr, function(item) {
+      return _.extend(item, _.find(gradesArr, { courseid: item.courseid }));
+    });
+    return transcript;
+  }
+  return { fetchUsersCourses, getUserGrades, getTranscript };
+}
+
+module.exports = transcriptService;
